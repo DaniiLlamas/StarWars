75e95fefb90e7f74c0c9e383f32ebabeaa1073da
DaniiLlamas <daniel.llamas.patxot.alu@iesjulioverne.es>
2024-01-26 12:09:07 +0100

    donete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Añadir al equipo', url: '/anadir', icon: 'mail' },
     { title: 'Listado de personajes', url: '/home', icon:'paper-plane'},
     { title: 'Mis favoritos', url: '/misfavs', icon:'paper-plane'}
   ];
